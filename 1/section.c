8246499b8e1a4da2e2c740311a5bdd97a5f190a6
QuanOnMintLatitude5400 <98805733+quan11304@users.noreply.github.com>
2025-04-02 16:46:54 +0700

    1 - Redefine macros and data types

--- 1/section.c
+++ 1/section.c
@@ -1,14 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define BYTE 1		// Corresponds to char
-#define WORD 2		// Corresponds to short
-#define DWORD 4		// Corresponds to long
-#define BYTEL 2
-#define WORDL 3
-#define DWORDL 5
+#define db 1		// Corresponds to char
+#define dw 2		// Corresponds to short
+#define dd 4		// Corresponds to long
+#define dbl 2
+#define dwl 3
+#define ddl 5
 
-typedef unsigned char uchar;
+typedef unsigned char BYTE;
+typedef unsigned short WORD;
+typedef unsigned long DWORD;
+
+// Ref: https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-image_data_directory
+typedef struct _IMAGE_DATA_DIRECTORY {
+    DWORD VirtualAddress;
+    DWORD Size;
+} IMAGE_DATA_DIRECTORY;
+
+// Ref: https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-image_file_header
+typedef struct _IMAGE_FILE_HEADER {
+    WORD  Machine;
+    WORD  NumberOfSections;
+    DWORD TimeDateStamp;
+    DWORD PointerToSymbolTable;
+    DWORD NumberOfSymbols;
+    WORD  SizeOfOptionalHeader;
+    WORD  Characteristics;
+} IMAGE_FILE_HEADER;
+
+// Ref: https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-image_optional_header32
+typedef struct _IMAGE_OPTIONAL_HEADER32 {
+    WORD                 Magic;
+    BYTE                 MajorLinkerVersion;
+    BYTE                 MinorLinkerVersion;
+    DWORD                SizeOfCode;
+    DWORD                SizeOfInitializedData;
+    DWORD                SizeOfUninitializedData;
+    DWORD                AddressOfEntryPoint;
+    DWORD                BaseOfCode;
+    DWORD                BaseOfData;
+    DWORD                ImageBase;
+    DWORD                SectionAlignment;
+    DWORD                FileAlignment;
+    WORD                 MajorOperatingSystemVersion;
+    WORD                 MinorOperatingSystemVersion;
+    WORD                 MajorImageVersion;
+    WORD                 MinorImageVersion;
+    WORD                 MajorSubsystemVersion;
+    WORD                 MinorSubsystemVersion;
+    DWORD                Win32VersionValue;
+    DWORD                SizeOfImage;
+    DWORD                SizeOfHeaders;
+    DWORD                CheckSum;
+    WORD                 Subsystem;
+    WORD                 DllCharacteristics;
+    DWORD                SizeOfStackReserve;
+    DWORD                SizeOfStackCommit;
+    DWORD                SizeOfHeapReserve;
+    DWORD                SizeOfHeapCommit;
+    DWORD                LoaderFlags;
+    DWORD                NumberOfRvaAndSizes;
+    IMAGE_DATA_DIRECTORY DataDirectory[16];
+} IMAGE_OPTIONAL_HEADER32;
+
+// Ref: https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-image_optional_header64
+typedef struct _IMAGE_OPTIONAL_HEADER64 {
+    WORD                 Magic;
+    BYTE                 MajorLinkerVersion;
+    BYTE                 MinorLinkerVersion;
+    DWORD                SizeOfCode;
+    DWORD                SizeOfInitializedData;
+    DWORD                SizeOfUninitializedData;
+    DWORD                AddressOfEntryPoint;
+    DWORD                BaseOfCode;
+    unsigned long long   ImageBase;
+    DWORD                SectionAlignment;
+    DWORD                FileAlignment;
+    WORD                 MajorOperatingSystemVersion;
+    WORD                 MinorOperatingSystemVersion;
+    WORD                 MajorImageVersion;
+    WORD                 MinorImageVersion;
+    WORD                 MajorSubsystemVersion;
+    WORD                 MinorSubsystemVersion;
+    DWORD                Win32VersionValue;
+    DWORD                SizeOfImage;
+    DWORD                SizeOfHeaders;
+    DWORD                CheckSum;
+    WORD                 Subsystem;
+    WORD                 DllCharacteristics;
+    unsigned long long   SizeOfStackReserve;
+    unsigned long long   SizeOfStackCommit;
+    unsigned long long   SizeOfHeapReserve;
+    unsigned long long   SizeOfHeapCommit;
+    DWORD                LoaderFlags;
+    DWORD                NumberOfRvaAndSizes;
+    IMAGE_DATA_DIRECTORY DataDirectory[16];
+} IMAGE_OPTIONAL_HEADER64;
 
 FILE *f;
 
@@ -19,13 +107,13 @@ int end(int status) {
     exit(status);
 }
 
-unsigned long getval(int size, int offset) {
-    uchar input[size];
+DWORD getval(int size, int offset) {
+    BYTE input[size];
     fseek(f, offset,SEEK_SET);
-    fread(input, 1,DWORD, f);
+    fread(input, 1,dd, f);
 
     // Convert from hex array to int
-    unsigned long var = 0;
+    DWORD var = 0;
     for (int i = 0; i < size; i++) {
         var = var | input[i] << (i * 8);
     }
@@ -44,22 +132,22 @@ int main(int argc, char *argv[]) {
     f = fopen(argv[1], "rb");
 
     // Verify magic byte
-    uchar magic[WORDL];
-    fread(magic, 1,WORD, f);
+    BYTE magic[dwl];
+    fread(magic, 1,dw, f);
     if (strcmp(magic, "MZ") != 0) {
         printf("Input error. File is not a PE executable.");
         end(1);
     }
 
-    unsigned long lfanew = getval(DWORD, 0x3C); // PE Header addres
-    unsigned short no_sections = getval(WORD, lfanew + 6);
-    unsigned short optional_size = getval(WORD, lfanew + 20);
-    unsigned long optional = lfanew + 24;
-    unsigned long entry = getval(DWORD, optional + 16);
+    DWORD lfanew = getval(dd, 0x3C); // PE Header addres
+    WORD no_sections = getval(dw, lfanew + 6);
+    WORD optional_size = getval(dw, lfanew + 20);
+    DWORD optional = lfanew + 24;
+    DWORD entry = getval(dd, optional + 16);
     // 0Bh 01h => optional_magic = 0x10B = 267 => 32-bit
     // 0Bh 02h => optional_magic = 0x20B = 523 => 64-bit
-    unsigned short optional_magic = getval(WORD, optional);
-    unsigned long sectbl1 = optional + optional_size;
+    WORD optional_magic = getval(dw, optional);
+    DWORD sectbl1 = optional + optional_size;
 
     char sections[no_sections][8 + 1];
     for (int i = 0; i < no_sections; i++) {
